bbcdc84e94a39f822dac8951eb3c4a6b8d81b3bd
paiain <paisin.chin62@sskru.ac.th>
2023-07-31 23:20:20 +0700

    up projec qrcode to oline

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,3 +1,14 @@
+
 from django.contrib import admin
+from webpage.models import LOGIN, crtateuser ,activity,Profile,add_ativity,add_data,add_data_all
 
 # Register your models here.
+
+admin.site.register(crtateuser)
+admin.site.register(LOGIN)
+admin.site.register(activity)
+admin.site.register(Profile)
+admin.site.register(add_ativity)
+admin.site.register(add_data)
+admin.site.register(add_data_all)
+
